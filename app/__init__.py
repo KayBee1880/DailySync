5469e0075cf3d2a506209d442477d5acd10190c1
Tereik Faulknor <tereikfaulknor@gmail.com>
2025-03-31 16:51:17 -0500

    comments on what to do in init.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
+'''This is where we will create the actual app and define its configurations,
+also register blueprints for routes we define in the routes folder
+'''
 def create_app():
     app = Flask(__name__,template_folder='templates',static_folder='static',static_url_path='/')
     
