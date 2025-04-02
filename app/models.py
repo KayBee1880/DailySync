4ed9ff4a1e4494b675957b4b33434fb7114a79eb
Tereik Faulknor <tereikfaulknor@gmail.com>
2025-04-02 17:36:51 -0500

    added more models to database

--- app/models.py
+++ app/models.py
@@ -19,11 +19,35 @@ class Habit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     name = db.Column(db.String(100), nullable=False)
-    frequency = db.Column(db.String(50), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    description = db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(50),nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
 class HabitLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     habit_id = db.Column(db.Integer, db.ForeignKey('habit.id'), nullable=False)
-    date_logged = db.Column(db.DateTime, default=datetime.utcnow)
+    date_logged = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     completed = db.Column(db.Boolean, default=False)
+
+class TrackedHabit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    habit_id = db.Column(db.Integer,db.ForeignKey('habit.id'),nullable=False)
+    date_logged = db.Column(db.DateTime)
+    date_logged = db.Column(db.DateTime)
+    streak = db.Column(db.Integer,default=0)
+    last_completed = db.Column(db.DateTime)
+
+class Milestones(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    habit_id = db.Column(db.Integer, db.ForeignKey('habit.id'), nullable=False)
+    habit_name = db.Column(db.String(255), nullable=False)
+    criteria = db.Column(db.String(125),nullable=False)
+
+class Goal(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'),nullable=False)
+    habit_id = db.Column(db.Integer,db.ForeignKey('habit.id'),nullable=False)
+    goal_type = db.Column(db.String(50),nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user
+from flask_login import login_user, login_required, current_user, logout_user
 from models import User
 from extensions import db
 
