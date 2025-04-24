a22db74679d6936f7df28fa46fe83ae5ff555dc0
Tereik Faulknor <tereikfaulknor@gmail.com>
2025-04-24 08:06:36 -0500

    confirmation email

--- app/forms.py
+++
@@ -1,7 +0,0 @@
-
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-class HabitForm(FlaskForm):
-    habit_name = StringField('Habit Name')
-    submit = SubmitField('Submit')
